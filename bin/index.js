457f74194ec9f558d16773961ceb80012968efa1
WumaCoder <15804854160@163.com>
2022-05-07 00:54:16 +0800

    chore: 准备发布

---
+++ bin/index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+try {
+  require("../dist/main");
+} catch (error) {
+  console.error(error);
+}
--- src/cmd/InitCommand.ts
+++ src/cmd/InitCommand.ts
@@ -1,7 +1,9 @@
 import { Logger } from "@deepkit/logger";
-import { cli, Command } from "@deepkit/app";
+import { cli, Command, flag } from "@deepkit/app";
 import { Config } from "../Config";
 import { ORMDatabase } from "../orm/ORMDatabase";
+import { mkdir } from "fs/promises";
+import { wrap } from "@tools";
 
 @cli.controller("init")
 export class InitCommand implements Command {
@@ -11,7 +13,12 @@ export class InitCommand implements Command {
     private orm: ORMDatabase
   ) {}
 
-  async execute() {
+  async execute(@flag force?: boolean) {
+    const [err] = await wrap(mkdir(this.config.runtimePath));
+    if (err && !force) {
+      this.logger.error(`Directory ${this.config.runtimePath} already exists.`);
+      return process.exit(0);
+    }
     await this.orm.migrate();
     return process.exit(0);
   }
